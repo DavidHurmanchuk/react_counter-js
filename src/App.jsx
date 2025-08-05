4fb2e9c8bf0d31f0789e9456919357ff8adcc106
David <davidhurmanchuk@gmail.com>
2025-08-05 09:40:23 +0300

    fix.2

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,13 @@ export const App = () => {
   const [count, setCount] = useState(0);
 
   const addOne = () => {
-    setCount(current => current + 1);
+    // write code here
+    setCount(prev => prev + 1);
   };
 
   const add100 = () => {
-    setCount(current => current + 100);
+    // write code here
+    setCount(prev => prev + 100);
   };
 
   // DON'T change the code below
